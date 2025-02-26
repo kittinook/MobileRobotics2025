982983481a5a6abe5e7d1bbd99f2c7f806a41362
kittinook <kitti.thamrong@gmail.com>
2025-02-26 22:08:12 +0700

    update

--- src/fra532_gazebo/src/cpp_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "fra532_gazebo/cpp_header.hpp"
-
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!\n";
-    return 0;
-}
--- src/fra532_slam/src/cpp_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "fra532_slam/cpp_header.hpp"
-
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!\n";
-    return 0;
-}
